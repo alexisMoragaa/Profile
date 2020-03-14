926de905b02332c6eccaff165500be83ff12c3a4
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-03-14 19:57:14 -0300

    Updates

---
+++ precache-manifest.162957a7e3ce1499aa9b2f1c7db53033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50aca7d7c99fa7af3e5a2e7dbe6d2e9e",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "8585a963ea263c2d1b04",
+    "url": "/Profile/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d70150224d1cb95fdd86",
+    "url": "/Profile/static/css/main.3990df75.chunk.css"
+  },
+  {
+    "revision": "8585a963ea263c2d1b04",
+    "url": "/Profile/static/js/2.296f88e6.chunk.js"
+  },
+  {
+    "revision": "d968270eef32431d826917628097d3be",
+    "url": "/Profile/static/js/2.296f88e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70150224d1cb95fdd86",
+    "url": "/Profile/static/js/main.0238a992.chunk.js"
+  },
+  {
+    "revision": "2fce13a40a65330037d7",
+    "url": "/Profile/static/js/runtime-main.1398e1c5.js"
+  }
+]);
