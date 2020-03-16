24d7f02bf85b69d418fa60160bba208749c627b8
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-03-15 23:36:15 -0300

    Updates

---
+++ precache-manifest.b1e443a13e1667b5faa3a55f69353901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39588702e51711462461af4d33fc3aaf",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "64c0c5a31f052a70faf7",
+    "url": "/Profile/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7c03f8e43863c2d91c86",
+    "url": "/Profile/static/css/main.543276a3.chunk.css"
+  },
+  {
+    "revision": "64c0c5a31f052a70faf7",
+    "url": "/Profile/static/js/2.8da1f239.chunk.js"
+  },
+  {
+    "revision": "d968270eef32431d826917628097d3be",
+    "url": "/Profile/static/js/2.8da1f239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c03f8e43863c2d91c86",
+    "url": "/Profile/static/js/main.629d2919.chunk.js"
+  },
+  {
+    "revision": "2fce13a40a65330037d7",
+    "url": "/Profile/static/js/runtime-main.1398e1c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Profile/precache-manifest.65a1a6508871c553f9708e0f00322ff7.js"
+  "/Profile/precache-manifest.b1e443a13e1667b5faa3a55f69353901.js"
 );
 
 self.addEventListener('message', (event) => {
