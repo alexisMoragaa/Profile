47d2406cdf7dcdc80eaf247a368fcd0003df88a9
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-03-17 00:31:21 -0300

    Updates

--- precache-manifest.df553fa1cdef3cbec21929ad0a8353f3.js
+++ precache-manifest.df553fa1cdef3cbec21929ad0a8353f3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92b4767613236d2bd50ae5536ebee6e1",
+    "revision": "d3f98a85881ac1126c960be4e62689a4",
     "url": "/Profile/index.html"
   },
   {
-    "revision": "821f6d4228658fc48a17",
+    "revision": "520bc7a8618d35e30585",
     "url": "/Profile/static/css/2.2990c7a7.chunk.css"
   },
   {
-    "revision": "128556aca4ddd1179511",
+    "revision": "461138e96afdbc7f01c4",
     "url": "/Profile/static/css/main.3990df75.chunk.css"
   },
   {
-    "revision": "821f6d4228658fc48a17",
-    "url": "/Profile/static/js/2.20686f1b.chunk.js"
+    "revision": "520bc7a8618d35e30585",
+    "url": "/Profile/static/js/2.468199e8.chunk.js"
   },
   {
     "revision": "3c5131a3c45a2e02e7444919d6d397ba",
-    "url": "/Profile/static/js/2.20686f1b.chunk.js.LICENSE.txt"
+    "url": "/Profile/static/js/2.468199e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "128556aca4ddd1179511",
-    "url": "/Profile/static/js/main.c392df5e.chunk.js"
+    "revision": "461138e96afdbc7f01c4",
+    "url": "/Profile/static/js/main.0eb1ab18.chunk.js"
   },
   {
     "revision": "2fce13a40a65330037d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Profile/precache-manifest.423fc2917b568d49c0a2f3b7a2137283.js"
+  "/Profile/precache-manifest.df553fa1cdef3cbec21929ad0a8353f3.js"
 );
 
 self.addEventListener('message', (event) => {
