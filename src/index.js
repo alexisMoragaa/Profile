3510879e550b0670a8be24c7ceb7cfe401838565
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-03-21 18:59:32 -0300

    Bug style fixed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 import $ from 'jquery'
 import jQuery from 'jquery'
 import Popper from 'popper.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/now-ui-kit.min.css';
 
 
